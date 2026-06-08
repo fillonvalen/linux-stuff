5c8a0009dd161514b27b8bc68bb7747d5af20674
fillonvalen <fillon.valen@gmail.com>
2026-06-08 14:19:05 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -4,12 +4,13 @@ cat void-alias >> ~/.bash_aliases
 sudo xbps-install time fzf wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad dwm st alacritty virtualbox-ose virtualbox-dkms clementine vlc dolphin dmenu eog gnome-calculator -y
 
 mv -v pinc/* /usr/include/
-rm -rv pinc/ 
 mv -v {pi,gl_notes} ~
 cd ~
 
 rm -rf linux-blog
 sudo xbps-remove -yoO
+find pi/ -name .gitkeep | xargs rm -v 
+sudo cp pi/pal/pcal /usr/bin/pal
 
 git clone https://github.com/shannpersand/comic-shanns && mv comic-shanns/v2/*.ttf /usr/share/fonts/
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage
