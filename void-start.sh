98f594ddf2bbd9278916ab248f77624c8a9bcee7
fillonvalen <fillon.valen@gmail.com>
2026-07-17 19:28:26 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -4,16 +4,6 @@ cat void-alias >> ~/.bash_aliases
 source ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.bashrc
 
-#installing 
-sudo xbps-install tmux time ncurses-libtinfo-devel libselinux-devel fzf fcft wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad dwm st alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin dmenu eog gnome-calculator -y
-
-#moving some stuff
-mv -v pinc/* /usr/include
-cat alias > ~/.bash_aliases
-cat void-alias >> ~/.bash_aliases
-source ~/.bash_aliases
-echo "source ~/.bash_aliases" >> ~/.bashrc
-
 #installing
 echo -en "\033[0;96mInstalling programs, cozy up...\033[0m"
 xbps-install wget tmux time ncurses-libtinfo-devel libselinux-devel fzf fcft wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad dwm st alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin dmenu eog gnome-calculator -y > /dev/null 2>&1
@@ -54,16 +44,3 @@ mv vi /usr/bin/mvi
 cd ~
 sudo cp ~/pi/pal/pcal /usr/bin/pal
 
-#cleaning
-rm -rf linux-stuff
-sudo xbps-remove -yoO
-find ~/pi/ -name .gitkeep | xargs rm -v
-
-#getting font and obsidian
-mkdir -v /usr/share/fonts
-git clone https://github.com/shannpersand/comic-shanns && mv comic-shanns/v2/*.ttf /usr/share/fonts/ && rm -rf comic-shanns && fc-cache -f
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage
-
-#creating folders
-mkdir -v Downloads Documents Pictures Videos
-mv -v Obsidian-1.12.7.AppImage Desktop/Obsidian
