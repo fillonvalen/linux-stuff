5c242f08a48fb6e91cd0d8a6656788aa4f45f123
fillonvalen <fillon.valen@gmail.com>
2026-07-17 22:54:03 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -10,9 +10,9 @@ sudo xbps-install wget tmux time ncurses-libtinfo-devel libselinux-devel fzf fcf
 
 #moving some stuff
 echo -en "\033[0;96mMoving and renaming stuff...\033[0m"
-sudo mv -v pinc/* /usr/include/
-mv -v pi ~
-mv -v gl_notes ~
+sudo mv pinc/* /usr/include/
+mv pi ~
+mv gl_notes ~
 mkdir -p ~/.config/alacritty/
 mv alacritty.toml ~/.config/alacritty/
 sudo rm /usr/bin/vi
@@ -24,17 +24,17 @@ sudo cp ~/pi/pal/pcal /usr/bin/pal
 #cleaning
 echo -en "\033[0;96mCleaning...\033[0m"
 rm -rf linux-stuff
-sudo xbps-remove -yoO
-find pi/ -name .gitkeep | xargs rm -v
+sudo xbps-remove -yoO > /dev/null 2>&1
+find pi/ -name .gitkeep > /dev/null 2>&1 | xargs rm
 
 #getting font and obsidian
-sudo mkdir -v /usr/share/fonts
+sudo mkdir /usr/share/fonts
 echo -en "\033[0;96mGetting comic shanns...\033[0m"
-git clone https://github.com/shannpersand/comic-shanns && mv comic-shanns/v2/*.ttf /usr/share/fonts/ && rm -rf comic-shanns && fc-cache -f
+git clone https://github.com/shannpersand/comic-shanns > /dev/null 2>&1 && sudo mv comic-shanns/v2/*.ttf /usr/share/fonts/ && rm -rf comic-shanns && fc-cache -f
 echo -en "\033[0;96mGetting Obsidian...\033[0m"
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage > /dev/null 2>&1
 
 #creating folders
 echo -en "\033[0;96mCreating folders\033[0m"
-mkdir -v Desktop Downloads Documents Pictures Videos
-mv -v Obsidian-1.12.7.AppImage Desktop/Obsidian
+mkdir Desktop Downloads Documents Pictures Videos
+mv Obsidian-1.12.7.AppImage Desktop/Obsidian
