5bf9e29cbd6fe80ada1398ad043d263bb47a2da6
fillonvalen <fillon.valen@gmail.com>
2026-07-31 13:56:53 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -1,21 +1,25 @@
 CYAN="\033[0;96m"
+WHT="\033[0;97m"
 CLR="\033[0m"
+OS=$(cat /etc/os-release | grep 'PRETTY_NAME' | sed -e "s/^.*=//g" -e 's/"//g')
+echo -en "This is a setup for your current OS: $(CYAN)$OS$(CLR).\n$(WHT)$(uname -s)$(CLR) version: $(CYAN)$(uname -r)$(CLR)\n"
 
 #alias
 cat alias > ~/.bash_aliases
-
 if [ $1 == "void" ]; then
   cat void-alias >> ~/.bash_aliases
+else 
+  cat apt-alias >> ~/.bash_aliases
 fi
-
 source ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.bashrc
 
 #installing
 echo -en "$(CYAN)Installing programs, cozy up...$(CLR)\n"
-
 if [ $1 == "void" ]; then
-  sudo xbps-install wget tmux pavucontrol time ncurses-libtinfo-devel libselinux-devel fzf fcft wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad i3 i3-status xorg xfce4-terminal alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin dmenu eog gnome-calculator -y > /dev/null 2>&1
+  sudo xbps-install wget tmux pavucontrol time ncurses-libtinfo-devel libselinux-devel fzf fcft libX11 libX11-devel dhcpcd unzip zip firefox leafpad i3 i3-status xorg dmenu xfce4-terminal alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin eog gnome-calculator -y > /dev/null 2>&1
+else
+  sudo apt install tmux wget time fzf firefox xfce4-terminal alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin -y > /dev/null 2>&1
 fi
 
 #moving some stuff
@@ -30,11 +34,12 @@ sudo chmod +x /usr/bin/vi
 sudo mv xs/pal /usr/bin/
 sudo chmod +x /usr/bin/pal
 
-#getting font and obsidian
+#creating fonts directory
 if [ ! -d /usr/share/fonts ]; then
   sudo mkdir /usr/share/fonts
 fi
 
+#getting obsidian
 echo -en "$(CYAN)Getting Obsidian...$(CLR)\n"
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage > /dev/null 2>&1
 
@@ -55,15 +60,19 @@ yes | sudo mv i3_stuff/i3status.conf /etc/i3status.conf
 cat i3_stuff/i3_conf_extra >> ~/.config/i3/config
 
 #setting .xinitrc and .Xresources
-echo -en "$(CYAN)Configuring .xinitrc and .Xresources...$(CLR)\n"
-echo -en "exec i3\nxrdb -merge .Xresources\nsource .bashrc" >> ~/.xinitrc
-echo -en "Xft.dpi: 196\nXcursor.theme: Adwaita\nXcursor.size: 32" >> ~/.Xresources
+if [ $1 == "void" ]; then
+  echo -en "$(CYAN)Configuring .xinitrc and .Xresources...$(CLR)\n"
+  echo -en "exec i3\nxrdb -merge .Xresources\nsource .bashrc" >> ~/.xinitrc
+  echo -en "Xft.dpi: 196\nXcursor.theme: Adwaita\nXcursor.size: 32" >> ~/.Xresources
+fi
 
 #cleaning
 echo -en "$(CYAN)Cleaning...$(CLR)\n"
 cd ~
 rm -rf ~/linux-stuff/
-sudo xbps-remove -yoO > /dev/null 2>&1
+if [ $1 == "void" ]; then
+  sudo xbps-remove -yoO > /dev/null 2>&1
+fi
 #find pi/ -name .gitkeep > /dev/null 2>&1 | xargs -I{} rm {}
 
 echo -en "$(CYAN)Ready!$(CLR)\n"
