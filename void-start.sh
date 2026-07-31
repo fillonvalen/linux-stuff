c1cfe06928b89bb26239f615daca543a15044c31
fillonvalen <fillon.valen@gmail.com>
2026-07-31 13:36:55 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -3,23 +3,32 @@ CLR="\033[0m"
 
 #alias
 cat alias > ~/.bash_aliases
-cat void-alias >> ~/.bash_aliases
+
+if [ $1 == "void" ]; then
+  cat void-alias >> ~/.bash_aliases
+fi
+
 source ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.bashrc
 
 #installing
 echo -en "$(CYAN)Installing programs, cozy up...$(CLR)\n"
-sudo xbps-install wget tmux pavucontrol time ncurses-libtinfo-devel libselinux-devel fzf fcft wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad i3 i3-status xorg xfce4-terminal alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin dmenu eog gnome-calculator -y > /dev/null 2>&1
+
+if [ $1 == "void" ]; then
+  sudo xbps-install wget tmux pavucontrol time ncurses-libtinfo-devel libselinux-devel fzf fcft wget libX11 libX11-devel dhcpcd unzip zip firefox leafpad i3 i3-status xorg xfce4-terminal alacritty virtualbox-ose virtualbox-ose-dkms clementine vlc dolphin dmenu eog gnome-calculator -y > /dev/null 2>&1
+fi
 
 #moving some stuff
 echo -en "$(CYAN)Moving and renaming stuff...$(CLR)\n"
 sudo mv pinc/* /usr/include/
 mv pi ~
 mv gl_notes ~
-sudo rm /usr/bin/vi
-sudo mv vi /usr/bin/
-sudo chmod +x /usr/bin/vi
 mv vimrc ~/.vimrc
+sudo rm /usr/bin/vi
+sudo mv xs/vi /usr/bin/
+sudo chmod +x /usr/bin/vi
+sudo mv xs/pal /usr/bin/
+sudo chmod +x /usr/bin/pal
 
 #getting font and obsidian
 if [ ! -d /usr/share/fonts ]; then
@@ -31,8 +40,14 @@ wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/O
 
 #creating folders
 echo -en "$(CYAN)Creating folders...$(CLR)\n"
-mkdir ~/{Desktop,Downloads,Documents,Pictures,Videos}
-mv Obsidian-1.12.7.AppImage ~/Desktop/Obsidian
+directories="Downloads Documents Desktop Pictures Videos"
+for dir in $directories; do
+        if [ ! -d ~/$dir ]; then
+                mkdir ~/$dir
+        fi
+done
+sudo mv Obsidian-1.12.7.AppImage /bin/osd
+sudo chmod +x /bin/osd
 
 #i3
 echo -en "$(CYAN)Configuring i3...$(CLR)\n"
