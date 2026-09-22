268d6bd4cfc7bf0ea2f17ea9e47d1ab9c8bc094b
fillonvalen <fillon.valen@gmail.com>
2026-09-22 14:01:41 -0300

    Update void-start.sh

--- void-start.sh
+++ void-start.sh
@@ -4,16 +4,11 @@ CLR="\033[0m"
 OS=$(cat /etc/os-release | grep 'PRETTY_NAME' | sed -e "s/^.*=//g" -e 's/"//g')
 echo -en "This is a setup for your current OS: $(CYAN)$OS$(CLR).\n$(WHT)$(uname -s)$(CLR) version: $(CYAN)$(uname -r)$(CLR)\n"
 
+chmod +x {alias,stuff-move,osd,tmux-conf}.sh
+
 #alias
 echo -en "$(CYAN)Setting alias...$(CLR)\n"
-cat alias/galias > ~/.bash_aliases
-if [ $1 == "void" ]; then
-  cat alias/void-alias >> ~/.bash_aliases
-else 
-  cat alias/apt-alias >> ~/.bash_aliases
-fi
-source ~/.bash_aliases
-echo "source ~/.bash_aliases" >> ~/.bashrc
+./alias.sh > /dev/null 2>&1  
 
 #installing
 echo -en "$(CYAN)Installing programs, cozy up...$(CLR)\n"
@@ -24,25 +19,15 @@ else
 fi
 
 #moving some stuff
-echo -en "$(CYAN)Moving and renaming stuff...$(CLR)\n"
-sudo mv pinc/* /usr/include/
-mv pi ~
-mv gl_notes ~
-mv vimrc ~/.vimrc
-sudo rm /usr/bin/vi
-sudo mv xs/vi /usr/bin/
-sudo chmod +x /usr/bin/vi
-sudo mv xs/pal /usr/bin/
-sudo chmod +x /usr/bin/pal
+./stuff-move.sh
 
-#creating fonts directory
+#creating fonts directory if doesn't exist
 if [ ! -d /usr/share/fonts ]; then
   sudo mkdir /usr/share/fonts
 fi
 
 #getting obsidian
-echo -en "$(CYAN)Getting Obsidian...$(CLR)\n"
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.AppImage > /dev/null 2>&1
+./osd.sh
 
 #creating folders
 echo -en "$(CYAN)Creating folders...$(CLR)\n"
@@ -52,8 +37,6 @@ for dir in $directories; do
                 mkdir ~/$dir
         fi
 done
-sudo mv Obsidian-1.12.7.AppImage /bin/osd
-sudo chmod +x /bin/osd
 
 #i3
 echo -en "$(CYAN)Configuring i3...$(CLR)\n"
@@ -68,16 +51,9 @@ if [ $1 == "void" ]; then
 fi
 
 #configuring tmux
-echo -en "$(CYAN)Configuring tmux...$(CLR)\n"
-mv tmux.conf ~/.tmux.conf
+./tmux-conf.sh
 
 #cleaning
-echo -en "$(CYAN)Cleaning...$(CLR)\n"
-cd ~
-rm -rf ~/linux-stuff/
-if [ $1 == "void" ]; then
-  sudo xbps-remove -yoO > /dev/null 2>&1
-fi
-#find pi/ -name .gitkeep > /dev/null 2>&1 | xargs -I{} rm {}
+./clean.sh
 
 echo -en "$(CYAN)Ready!$(CLR)\n"
